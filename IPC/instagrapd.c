e81a66600889d714e2bf43feace2c47aebe35105
sungbin <21700613@handong.edu>
2019-04-25 02:00:56 +0900

    link submitter--instagrapd--worker

--- IPC/instagrapd.c
+++ IPC/instagrapd.c
@@ -1,59 +1,190 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-#define BUFFER_SIZE 256
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
 
-char ip[50] = "";
-char wport[50] = "";
-char port[10] = "";
+char wip[50] = "";
+char wport[10] = "";
 char* dir_name;
+char port[10] = "";
+int send_to_worker(char const* ip_address, int port, char const* message);
 
-typedef struct* Submission pSubmission;
-typedef struct Submission {
-	char id[50];
-	char pw[50];
-	char source[100001];
-
-} Submission;
-
+void receive();
 void cli(int argc, char const *argv[]) {
-	int i;
+        int i;
         char* wip_port;
 
-	for(i = 1; i < argc; i+=2) {
+        for(i = 1; i < argc; i+=2) {
                 if(strcmp("-w", argv[i]) == 0) {
                         wip_port = argv[i+1];
                 } else if(strcmp("-p", argv[i]) == 0) {
-                        port = argv[i+1];
+                        strcpy(port,argv[i+1]);
                 } else {
                         dir_name = argv[i];
                 }
         }
-	for(i = 0; wip_port[i] != ':'; i++) {
+        for(i = 0; wip_port[i] != ':'; i++) {
                 char temp[5] = "";
                 sprintf(temp,"%c",wip_port[i]);
-                strcat(ip,temp);
+                strcat(wip,temp);
         }
         i++;
-	for(; i< strlen(wip_port); i++) {
+        for(; i< strlen(wip_port); i++) {
                 char temp[5] = "";
                 sprintf(temp,"%c",wip_port[i]);
                 strcat(wport,temp);
         }
 }
 
-int
-main(int argc, char const *argv[])
+void
+child_proc(int conn)
+{
+	char buf[1024] ;
+	char * data = 0x0, * orig = 0x0 ;
+	int len = 0 ;
+	int s ;
+
+	while ( (s = recv(conn, buf, 1023, 0)) > 0 ) {
+		buf[s] = 0x0 ;
+		if (data == 0x0) {
+			data = strdup(buf) ;
+			len = s ;
+		}
+		else {
+			data = realloc(data, len + s + 1) ;
+			strncpy(data + len, buf, s) ;
+			data[len + s] = 0x0 ;
+			len += s ;
+		}
+
+	}
+	printf(">%s\n", data) ;
+	
+	orig = data ;
+//	sleep(6); //after tests
+
+/* This logic that get test result from worker, and send to submitter */
+	char message[1000001];
+	
+	strcpy(message,data);
+
+
+	int fd_worker = send_to_worker(wip,atoi(wport),message);
+	
+	data = 0x0 ;
+	len = 0 ;
+	while ( (s = recv(fd_worker, buf, 1023, 0)) > 0 ) {
+		buf[s] = 0x0 ;
+		if (data == 0x0) {
+			data = strdup(buf) ;
+			len = s ;
+		}
+		else {
+			data = realloc(data, len + s + 1) ;
+			strncpy(data + len, buf, s) ;
+			data[len + s] = 0x0 ;
+			len += s ;
+		}
+
+	} // data is feedback of worker
+/**/
+	while (len > 0 && (s = send(conn, data, len, 0)) > 0) { //back to submitter
+		data += s ;
+		len -= s ;
+	}
+	shutdown(conn, SHUT_WR) ;
+	if (orig != 0x0) 
+		free(orig) ;
+}
+
+int 
+main(int argc, char const *argv[]) 
 {
 	cli(argc,argv);
-	/*
-	printf("dir_name: %s\n",file_name);
-	printf("ip: %s\n",ip);
-	printf("port: %s\n",port);
-	wport
-	*/
-	
+	receive();
+} 
+void receive() {
+	int listen_fd, new_socket ;
+        struct sockaddr_in address;
+        int opt = 1;
+        int addrlen = sizeof(address);
+
+        char buffer[1024] = {0};
+
+        listen_fd = socket(AF_INET /*IPv4*/, SOCK_STREAM /*TCP*/, 0 /*IP*/) ;
+        if (listen_fd == 0)  {
+                perror("socket failed : ");
+                exit(EXIT_FAILURE);
+        }
+
+        memset(&address, '0', sizeof(address));
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY /* the localhost*/ ;
+        address.sin_port = htons(atoi(port));
+        if (bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+                perror("bind failed : ");
+                exit(EXIT_FAILURE);
+        }
+
+        while (1) {
+                if (listen(listen_fd, 16 /* the size of waiting queue*/) < 0) {
+                        perror("listen failed : ");
+                        exit(EXIT_FAILURE);
+                }
+
+                new_socket = accept(listen_fd, (struct sockaddr *) &address, (socklen_t*)&addrlen) ;
+                if (new_socket < 0) {
+                        perror("accept");
+                        exit(EXIT_FAILURE);
+                }
+
+                if (fork() > 0) {
+                        child_proc(new_socket) ;
+                }
+                else {
+                        close(new_socket) ;
+                }
+        }
+}
+int send_to_worker(char const* wip_address, int wport, char const* message) {
+	struct sockaddr_in serv_addr; 
+	int sock_fd ;
+	int s, len ;
+	char buffer[1024] = {0}; 
+	char * data ;
+
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0) ;
+        if (sock_fd <= 0) {
+                perror("socket failed : ") ;
+                exit(EXIT_FAILURE) ;
+        }
+
+        memset(&serv_addr, '0', sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(wport);
+        if (inet_pton(AF_INET, wip_address, &serv_addr.sin_addr) <= 0) {
+                perror("inet_pton failed : ") ;
+                exit(EXIT_FAILURE) ;
+        }
+
+        if (connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+                perror("connect failed : ") ;
+                exit(EXIT_FAILURE) ;
+        }
+
+//      scanf("%s", buffer) ;
+        strcpy(buffer,message);
+
+        data = buffer ;
+        len = strlen(buffer) ;
+        s = 0 ;
+        while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
+                data += s ;
+                len -= s ;
+        }
+
+        shutdown(sock_fd, SHUT_WR) ;
+	return sock_fd;
 }
--- IPC/submitter.c
+++ IPC/submitter.c
@@ -12,6 +12,9 @@
 #include <sys/time.h>
 #include <signal.h>
 
+void submit(char const* ip_address, int port, char const* message);
+void read_file(char const* file_name);
+void cli(int argc, char const *argv[]);
 char ip[50] = "";
 char port[10] = "";
 char* user_id;
@@ -19,11 +22,62 @@ char* user_pw;
 char* file_name;
 char result[100001] = "";
 char message[1000001] = "";
-int pipes[2] ;
 int sock_fd;
+struct sockaddr_in serv_addr; 
+int s, len ;
+char buffer[1024] = {0}; 
+char * data ;
 
-void read_file(char const* file_name);
-void handler(int sig);
+void submit(char const* ip_address, int port, char const* message) {
+        sock_fd = socket(AF_INET, SOCK_STREAM, 0) ;
+        if (sock_fd <= 0) {
+                perror("socket failed : ") ;
+                exit(EXIT_FAILURE) ;
+        }
+        
+        memset(&serv_addr, '0', sizeof(serv_addr));
+        serv_addr.sin_family = AF_INET; 
+        serv_addr.sin_port = htons(port); 
+        if (inet_pton(AF_INET, ip_address, &serv_addr.sin_addr) <= 0) {
+                perror("inet_pton failed : ") ;
+                exit(EXIT_FAILURE) ;
+        }
+        
+        if (connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+                perror("connect failed : ") ;
+                exit(EXIT_FAILURE) ;
+        }
+
+//      scanf("%s", buffer) ;
+        strcpy(buffer,message);
+        
+        data = buffer ;
+        len = strlen(buffer) ;
+        s = 0 ;
+        while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
+                data += s ;
+                len -= s ;
+        }
+        
+        shutdown(sock_fd, SHUT_WR) ;
+
+}
+
+void read_file(char const* file_name) {
+
+	FILE *fp;
+	char buffer[BUFFER_SIZE + 1];
+
+	if ((fp = fopen(file_name, "r+")) != NULL) {
+		memset(buffer, 0, sizeof(buffer));
+		while (fread(buffer, 1, BUFFER_SIZE, fp) != 0) {
+			strncat(result,buffer,BUFFER_SIZE);
+		}
+		fclose(fp);
+	}
+//	printf("%s",result);
+}
+
 void cli(int argc, char const *argv[]) {
 	int i;
         char* ip_port;
@@ -50,10 +104,8 @@ void cli(int argc, char const *argv[]) {
                 sprintf(temp,"%c",ip_port[i]);
                 strcat(port,temp);
         }
-}
 
-void submit(char const* ip_address, int port, char const* message) {
-        strcat(message,"<id>\n");
+	strcat(message,"<id>\n");
         strcat(message,user_id);
         strcat(message,"\n");
         strcat(message,"<pw>\n");
@@ -66,95 +118,11 @@ void submit(char const* ip_address, int port, char const* message) {
         strcat(message,result);
 
         strcat(message,"\n<end>");
-
-	struct sockaddr_in serv_addr; 
-//	int sock_fd ;
-	int s, len ;
-	char buffer[1024] = {0}; 
-	char * data ;
-	
-	sock_fd = socket(AF_INET, SOCK_STREAM, 0) ;
-	if (sock_fd <= 0) {
-		perror("socket failed : ") ;
-		exit(EXIT_FAILURE) ;
-	} 
-
-	memset(&serv_addr, '0', sizeof(serv_addr)); 
-	serv_addr.sin_family = AF_INET; 
-	serv_addr.sin_port = htons(port); 
-	if (inet_pton(AF_INET, ip_address, &serv_addr.sin_addr) <= 0) {
-		perror("inet_pton failed : ") ; 
-		exit(EXIT_FAILURE) ;
-	} 
-
-	if (connect(sock_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		perror("connect failed : ") ;
-		exit(EXIT_FAILURE) ;
-	}
-
-	strcpy(buffer, message);
-	//scanf("%s", buffer) ;
-	
-	data = buffer ;
-	len = strlen(buffer) ;
-	s = 0 ;
-	while (len > 0 && (s = send(sock_fd, data, len, 0)) > 0) {
-		data += s ;
-		len -= s ;
-	}
-
-	shutdown(sock_fd, SHUT_WR) ;
-
-	char buf[1024] ;
-	data = 0x0 ;
-	len = 0 ;
-	while ( (s = recv(sock_fd, buf, 1023, 0)) > 0 ) {
-		buf[s] = 0x0 ;
-		if (data == 0x0) {
-			data = strdup(buf) ;
-			len = s ;
-		}
-		else {
-			data = realloc(data, len + s + 1) ;
-			strncpy(data + len, buf, s) ;
-			data[len + s] = 0x0 ;
-			len += s ;
-		}
-
-	}
-	shutdown(sock_fd, SHUT_WR) ;
-	printf("fd: %d\n",sock_fd);
 }
 
-void read_file(char const* file_name) {
-
-	FILE *fp;
-	char buffer[BUFFER_SIZE + 1];
-
-	if ((fp = fopen(file_name, "r+")) != NULL) {
-		memset(buffer, 0, sizeof(buffer));
-		while (fread(buffer, 1, BUFFER_SIZE, fp) != 0) {
-			strncat(result,buffer,BUFFER_SIZE);
-		}
-		fclose(fp);
-	}
-//	printf("%s",result);
-}
-
-void
-child_proc(int conn) {
-	
-	
-	
-}
-
-//wating until receive message from instagrapd
 void
 parent_proc(int sock_fd) {
-	int s, len ;
-	char buffer[1024] = {0}; 
 	char buf[1024] ;
-	char * data ;
 	data = 0x0 ;
 	len = 0 ;
 	while ( (s = recv(sock_fd, buf, 1023, 0)) > 0 ) {
@@ -173,35 +141,38 @@ parent_proc(int sock_fd) {
 	}
 	printf(">%s\n", data); 	
 }
-void handler(int sig) {
-	parent_proc(sock_fd) ;
 
+void receive() {
+	char buf[1024] ;
+        data = 0x0 ;
+        len = 0 ;
+        while ( (s = recv(sock_fd, buf, 1023, 0)) > 0 ) {
+                buf[s] = 0x0 ;
+                if (data == 0x0) {
+                        data = strdup(buf) ;
+                        len = s ;
+                }
+                else {
+                        data = realloc(data, len + s + 1) ;
+                        strncpy(data + len, buf, s) ;
+                        data[len + s] = 0x0 ;
+                        len += s ;
+                }
+
+        }
+        printf(">%s\n", data);
 }
-int
-main(int argc, char const *argv[])
-{
-	
+
+int 
+main(int argc, char const *argv[]) 
+{ 
 	cli(argc,argv);
-	
-	pid_t child_pid ;
-	int exit_code ;
-
-	if (pipe(pipes) != 0) {
-		perror("Error") ;
-		exit(1) ;
-	}
-
+	submit(ip,atoi(port),message); //ip, port, message`
+	receive();
+/*	
 	submit(ip,atoi(port),message); //ip, port, message
 	parent_proc(sock_fd) ;
-	/*
-	struct itimerval t ;
-	signal(SIGALRM, handler) ;
-	t.it_value.tv_sec = 1 ;
-	t.it_value.tv_usec = 100000 ; // 1.1 sec
-	t.it_interval = t.it_value ;
-	setitimer(ITIMER_REAL, &t, 0x0) ;
-	while (1) ;
-	*/
-	exit(0) ;
-	
-}
+	exit(0);
+*/
+} 
+
