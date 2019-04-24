b6a299fbbb8f83c02738429783f6be1ac6803227
sungbin <37574978+sungbin@users.noreply.github.com>
2019-04-24 12:21:44 +0900

    Update submitter.c

--- IPC/submitter.c
+++ IPC/submitter.c
@@ -140,10 +140,112 @@ void read_file(char const* file_name) {
 //	printf("%s",result);
 }
 
+void
+child_proc(int conn)
+{
+	char buf[1024] ;
+	char * data = 0x0, * orig = 0x0 ;
+	int len = 0 ;
+	int s ;
+
+	while ( (s = recv(conn, buf, 1023, 0)) > 0 ) {
+		buf[s] = 0x0 ;
+		if (data == 0x0) {
+			data = strdup(buf) ;
+			len = s ;
+		}
+		else {
+			data = realloc(data, len + s + 1) ;
+			strncpy(data + len, buf, s) ;
+			data[len + s] = 0x0 ;
+			len += s ;
+		}
+
+	}
+	printf(">%s\n", data) ;
+	
+	orig = data ;
+	while (len > 0 && (s = send(conn, data, len, 0)) > 0) {
+		data += s ;
+		len -= s ;
+	}
+	shutdown(conn, SHUT_WR) ;
+	if (orig != 0x0) 
+		free(orig) ;
+}
+
+//wating until receive message from instagrapd
+void
+parent_proc(int port_id)
+{
+	int listen_fd, new_socket ; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address); 
+
+	char buffer[1024] = {0}; 
+
+	listen_fd = socket(AF_INET /*IPv4*/, SOCK_STREAM /*TCP*/, 0 /*IP*/) ;
+	if (listen_fd == 0)  { 
+		perror("socket failed : "); 
+		exit(EXIT_FAILURE); 
+	}
+	
+	memset(&address, '0', sizeof(address)); 
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY /* the localhost*/ ; 
+	address.sin_port = htons(port_id); 
+	if (bind(listen_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		perror("bind failed : "); 
+		exit(EXIT_FAILURE); 
+	} 
+
+	while (1) {
+		if (listen(listen_fd, 16 /* the size of waiting queue*/) < 0) { 
+			perror("listen failed : "); 
+			exit(EXIT_FAILURE); 
+		} 
+
+		new_socket = accept(listen_fd, (struct sockaddr *) &address, (socklen_t*)&addrlen) ;
+		if (new_socket < 0) {
+			perror("accept"); 
+			exit(EXIT_FAILURE); 
+		} 
+
+		if (fork() > 0) {
+			child_proc(new_socket) ;
+			exit(0);
+		}
+		else {
+			close(new_socket) ;
+		}
+	}
+}
+
 int
 main(int argc, char const *argv[])
 {
+	
 	cli(argc,argv);
-	submit(ip,atoi(port),message); //ip, port, message
+	
+	pid_t child_pid ;
+	int exit_code ;
+
+	if (pipe(pipes) != 0) {
+		perror("Error") ;
+		exit(1) ;
+	}
+	printf("%d %d\n", pipes[0], pipes[1]) ;
+
+	child_pid = fork() ;
+	if (child_pid == 0) {
+		submit(ip,atoi(port),message); //ip, port, message
+	}
+	else {
+		parent_proc(atoi(port)) ;
+	}
+	wait(&exit_code) ;
+
+	exit(0) ;
 	
 }
