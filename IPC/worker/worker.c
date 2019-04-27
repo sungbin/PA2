88a01c874960bafaded66005bb84823e9bb0dd48
sungbin <21700613@handong.edu>
2019-04-27 21:25:10 +0900

    make a function

--- IPC/worker/worker.c
+++ IPC/worker/worker.c
@@ -9,11 +9,19 @@
 
 void receive();
 void child_proc(int conn);
-void test_a_case(char output[], char content[], char in_name[], char in_content[]);
+void test_a_case(char output[], char content[], char in_name[], char in_content[], char program[], char program_c[]);
 
-
+void function(char* test_f_name, char* _content, char* message, char* test_in_str);
 char content[1000001];
 char port[10] = "";
+
+struct Arg {
+	char test_f_name[5000];
+	char _content[5000];
+	char test_in_str[5000];
+	char* message;
+};
+
 
 int
 main(int argc, char const *argv[])
@@ -92,8 +100,10 @@ child_proc(int conn)
 //        printf(">%s\n", data) ;
     char message[1000001] = "";
     char *ptr = strtok(data, "\n");      // " " 공백 문자를 기준으로 문자열을 자름, 포인터 반환
-    char test_f_name[50] = "";
+    char test_f_name[5000] = "";
     char test_in_str[1000001] = "";
+    struct Arg* args[6000];
+    int _count = 0;
     while (ptr != NULL)               // 자른 문자열이 나오지 않을 때까지 반복
     {
         //printf("%s\n", ptr);          // 자른 문자열 출력
@@ -123,61 +133,28 @@ child_proc(int conn)
         	} while(ptr != NULL && strcmp("<id>",ptr) != 0);
 
         //add THREAD
-		int leng = strlen(test_f_name);
-		int i,div;
-		char str[50] = "";
-		for(i = leng-1; i >= 0; i--) {
-			if(test_f_name[i] == '/' || test_f_name[i] == '\\') {
-				div = i;
-				break;
-			}
-		}
-		for(i = div+1; i<leng; i++) {
+		struct Arg* arg;
+		arg = (struct Arg*) malloc(sizeof(struct Arg));
+		strcpy(arg->test_f_name,test_f_name);
+		strcpy(arg->_content,_content);
+		strcpy(arg->test_in_str,test_in_str);
+		arg->message = message;
+		args[_count] = arg;
 
-			char temp[5] = "";
-                	sprintf(temp,"%c",test_f_name[i]);
-                	strcat(str,temp);
-		}
-
-		//make program with content
-		system("exec rm program program.c");
-			//1. make program.c
-		if(fork() == 0)            //creating 2nd child
-        	{
-            		close(STDIN_FILENO);   //closing stdin
-
-			int fd = open("program.c", O_WRONLY | O_CREAT, 0644) ;
-			close(STDOUT_FILENO);
-			dup(fd);
-			close(fd) ;
-
-			printf("%s",_content);
-        	    exit(1);
-        	} else {
-              	 	int exit_code ;
-              	 	wait(&exit_code) ;
-        	}
-			//2. compile program.c as program
-		system("exec gcc program.c -o program");
-		int nResult = access( "./program", 0 );
-		if( nResult == -1 )
-		{
-			//TODO: build fail Error!
-			strcpy(message,"build fail\n");
-			break;
-		}
-
-		//test		
-		char output[100001];
-		test_a_case(output, _content, str, test_in_str);
-		strcat(message,output);
+//		function(args[_count]->test_f_name, args[_count]->_content,args[_count]->message, args[_count]->test_in_str);
+		_count++;
 	}
 	
 	//add THREAD
 
         ptr = strtok(NULL, "\n");      // 다음 문자열을 잘라서 포인터를 반환
     }	
-       
+         
+	for(int i = 0; i<_count; i++) {
+	//	printf("test_f_name:%s\n",args[i]->test_f_name);
+		function(args[i]->test_f_name, args[i]->_content,args[i]->message, args[i]->test_in_str);
+	}
+
         orig = data ;
         strcpy(data,message);
 /**/    
@@ -191,7 +168,7 @@ child_proc(int conn)
                 free(orig) ;
 }
 
-void test_a_case(char output[], char content[], char in_name[], char in_content[]) {
+void test_a_case(char output[], char content[], char in_name[], char in_content[], char program[], char program_c[]) {
 	char result[100001] = "";
 
 
@@ -205,7 +182,7 @@ void test_a_case(char output[], char content[], char in_name[], char in_content[
 
 	// name of output
 	int div;
-	char out_name[50];
+	char out_name[5000];
 	for(int i = 0; i < strlen(in_name); i++) {
 		if(in_name[i] == '.') {
 			div = i;
@@ -227,7 +204,7 @@ void test_a_case(char output[], char content[], char in_name[], char in_content[
 		close(STDOUT_FILENO);
 		dup(fd);
 		close(fd);
-		execl("./applier", "./applier", out_name, "program", in_content, (char *) 0x0) ;
+		execl("./applier", "./applier", out_name, program, in_content, (char *) 0x0) ;
         	exit(1);
         } else {
 		sleep(3);
@@ -260,11 +237,92 @@ void test_a_case(char output[], char content[], char in_name[], char in_content[
 	strcat(t_command," ");
 	strcat(t_command,out_name);
 	strcat(t_command," ");
-	strcat(t_command,"program");	
+	strcat(t_command,program);	
 	strcat(t_command," ");
-	strcat(t_command,"program.c");
+	strcat(t_command,program_c);
 	
 	system(t_command); //clear command
 
 	strcpy(output,result);
+}
+void function(char* test_f_name, char* _content, char* message, char* test_in_str) {
+
+	int leng = strlen(test_f_name);
+		int i,div;
+		char str[5000] = "";
+		for(i = leng-1; i >= 0; i--) {
+			if(test_f_name[i] == '/' || test_f_name[i] == '\\') {
+				div = i;
+				break;
+			}
+		}
+		for(i = div+1; i<leng; i++) {
+
+			char temp[5] = "";
+                	sprintf(temp,"%c",test_f_name[i]);
+                	strcat(str,temp);
+		}
+		for(i = 0; i<strlen(str);i++) {
+			if(str[i] == '.') {
+				div = i;
+			}
+		}
+		char number[5000];
+		strncpy(number,str,div);
+//		printf("number: %s\n",number);
+		char program[6000] = "program";
+		strcat(program,number);
+		char program_c[6000];
+		strcpy(program_c,program);
+		strcat(program_c,".c");
+
+		char rm_1[5000];
+		strcpy(rm_1,"exec rm ");
+		strcat(rm_1,program);
+		strcat(rm_1," ");
+		strcat(rm_1,program_c);
+
+		system(rm_1);
+
+		//make program with content
+			//1. make program.c
+		if(fork() == 0)            //creating 2nd child
+        	{
+            		close(STDIN_FILENO);   //closing stdin
+
+			int fd = open(program_c, O_WRONLY | O_CREAT, 0644) ;
+			close(STDOUT_FILENO);
+			dup(fd);
+			close(fd) ;
+
+			printf("%s",_content);
+        	    exit(1);
+        	} else {
+              	 	int exit_code ;
+              	 	wait(&exit_code) ;
+        	}
+			//2. compile program.c as program
+
+		char rm_2[5000];
+                strcpy(rm_2,"exec gcc -o ");
+                strcat(rm_2,program);
+                strcat(rm_2," ");
+                strcat(rm_2,program_c);
+
+		system(rm_2);
+		
+		char ar_3[5000];
+		strcpy(ar_3,"./");
+		strcat(ar_3,program);
+		int nResult = access( ar_3, 0 );
+		if( nResult == -1 )
+		{
+			strcpy(message,"build fail\n");
+		}
+		else {
+		//test		
+			char output[100001];
+			test_a_case(output, _content, str, test_in_str,program,program_c);
+			strcat(message,output);
+		}
 }
